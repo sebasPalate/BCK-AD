d21b074b3aee0edff328896bcc6150dd54b1f89c
Sebastián Palate <75347523+sebasPalate@users.noreply.github.com>
2023-07-12 01:47:52 -0500

    Change Name

--- models/loan.js
+++ models/loan.js
@@ -160,7 +160,7 @@ class Loan {
         const sql = `
             SELECT
                 p.id_prestamo,
-                u.nombre,
+                e.nombre_equipo,
                 e.marca,
                 p.fecha_prestamo,
                 u.nombre,
