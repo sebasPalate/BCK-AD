82e72d4678d52ac8e57a8c25eeef78050d816176
Sebastián Palate <75347523+sebasPalate@users.noreply.github.com>
2023-07-15 22:41:46 -0500

    Up

--- models/loan.js
+++ models/loan.js
@@ -106,7 +106,7 @@ class Loan {
     // Crear Prestamo
     create(id_equipo_per, id_usuario_solicita_per) {
         const estado = "PENDIENTE";
-        const id_usuario_presta_per = null; // Usuario Laboratorio por defecto
+        const id_usuario_presta_per = 1; // Usuario Laboratorio por defecto
 
         const sql = `
             INSERT INTO prestamo (id_equipo_per, id_usuario_presta_per, id_usuario_solicita_per, estado) VALUES (?, ?, ?, ?)
